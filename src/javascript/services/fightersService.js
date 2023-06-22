9cc7f52d20f6624e5cbeea7013ab244e9899e604
unknown <poleluk@ukr.net>
2023-06-22 21:09:01 +0300

    Added getFighterDetails (method) in FighterService

--- src/javascript/services/fightersService.js
+++ src/javascript/services/fightersService.js
@@ -9,14 +9,20 @@ class FighterService {
             return apiResult;
         } catch (error) {
             throw error;
-        }
-    }
+        };
+    };
 
     async getFighterDetails(id) {
-        // todo: implement this method
-        // endpoint - `details/fighter/${id}.json`;
-    }
-}
+        try {
+            const endpoint = `details/fighter/${id}.json`;
+            const apiResult = await callApi(endpoint, 'GET');
+      
+            return apiResult;
+        } catch (error) {
+            throw error;
+        };
+    };
+};
 
 const fighterService = new FighterService();
 
