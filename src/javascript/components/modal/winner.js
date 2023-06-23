a27a20106cad8f24adaafee39adac419a5e9352c
unknown <poleluk@ukr.net>
2023-06-23 05:25:05 +0300

    + finish version

--- src/javascript/components/modal/winner.js
+++ src/javascript/components/modal/winner.js
@@ -1,9 +1,21 @@
 import showModal from './modal';
-import { createFighterImage } from '../fighterPreview';
+import createElement from '../../helpers/domHelper';
 
 export default function showWinnerModal(fighter) {
+    const { source, name } = fighter;
+    const attributes = {
+        src: source,
+        title: name,
+        alt: name
+    };
+
+    const bodyElement = createElement({
+        tagName: 'img',
+        className: 'modal-body',
+        attributes
+    });
+
     const title = `${fighter.name.toUpperCase()} WIN!`;
-    const bodyElement = createFighterImage(fighter);
 
     showModal({ title, bodyElement });
 }
